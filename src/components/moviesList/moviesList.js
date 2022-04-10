563b8fcf182a995c1de6c0973938ffd83942c029
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-10 21:31:57 +0200

    add GET request for movies list

--- src/components/moviesList/moviesList.js
+++ src/components/moviesList/moviesList.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MovieCard } from '../movieCard/movieCard';
+import axios from 'axios';
 
 export function MoviesList() {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/movies').then((movies) => {
+      setMovies(movies.data);
+    });
+  }, []);
+
   return (
     <div className='movies-list'>
       {movies.map((movie) => {
