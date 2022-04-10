b51e2b33854c230fc102b4627ae7667906684c1b
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-10 22:04:11 +0200

    update movie card layout

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './movieCard.scss';
 
 export function MovieCard({ movie }) {
   return (
     <div className='movie-card' key={movie.id}>
       <img src={movie.image_url} alt=''></img>
-      <h6>{movie.title}</h6>
-      <p>{movie.genre}</p>
-      <p>{movie.year}</p>
+      <div className='movie-info'>
+        <h6>{movie.title}</h6>
+        <p>{movie.genre}</p>
+        <p>{movie.year}</p>
+      </div>
     </div>
   );
 }
