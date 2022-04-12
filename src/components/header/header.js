3ae7b7ac041db067c8eeaad275920d35c24c28bb
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-12 21:13:48 +0200

    add header styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import './header.scss';
 
 export function Header() {
   return (
     <header>
+      <NavLink to='/' className='logo'>
+        MOVIES APP
+      </NavLink>
       <nav>
-        <NavLink to='/'>Movies app</NavLink>
-        <div>
-          <NavLink to='/add-movie'>Add a movie</NavLink>
-        </div>
+        <NavLink to='/add-movie'>ADD A MOVIE</NavLink>
         <Outlet></Outlet>
       </nav>
     </header>
