1ce19758ef334dbc7a4735f2d9c781f91a2957e9
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-14 14:07:52 +0200

    add routes for movie modal view

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AddMovieForm } from './components/addMovieForm/addMovieForm';
+import { ViewMovie } from './components/viewMovie/viewMovie';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}></Route>
-        <Route path='add-movie' element={<AddMovieForm />}></Route>
+        <Route path='/' element={<App />}>
+          <Route path='movie/:id' element={<ViewMovie />}></Route>
+        </Route>
+        <Route path='/add-movie' element={<AddMovieForm />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
