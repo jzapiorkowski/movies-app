ba22e0547443ccde95af8126b47408633eac985d
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-14 21:35:50 +0200

    add links to modal movie view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { MoviesList } from './components/moviesList/moviesList';
 import { Header } from './components/header/header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Header></Header>
       <MoviesList></MoviesList>
+      <Outlet />
     </div>
   );
 }
--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './movieCard.scss';
 import Checkbox from '@mui/material/Checkbox';
+import { Link } from 'react-router-dom';
 
 export function MovieCard({ handleChecked, movie }) {
   const handleChange = () => {
@@ -9,12 +10,16 @@ export function MovieCard({ handleChecked, movie }) {
 
   return (
     <div className='movie-card' key={movie.id}>
-      <img src={movie.image_url} alt=''></img>
+      <Link to={`movie/${movie.id}`}>
+        <img src={movie.image_url} alt=''></img>
+      </Link>
       <div className='movie-info'>
-        <div className='title'>
-          {movie.title}
-          <span>{movie.year}</span>
-        </div>
+        <Link to={`movie/${movie.id}`}>
+          <div className='title'>
+            {movie.title}
+            <span>{movie.year}</span>
+          </div>
+        </Link>
         <p>genre: {movie.genre}</p>
       </div>
       <div className='right-section'>
