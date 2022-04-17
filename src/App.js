bc76901ed03b5b21daa8505f960f944271eda964
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-17 22:23:56 +0200

    add favorite movie context provider

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { Header } from './components/header/header';
 import { Route, Routes } from 'react-router-dom';
 import { AddMovieForm } from './routes/addMovieForm/addMovieForm';
 import { ViewMovie } from './routes/viewMovie/viewMovie';
+import { FavoriteMovieProvider } from './contexts/favoriteMovieContext';
 
 function App() {
   return (
     <div className='App'>
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<MoviesList />}>
-          <Route path='movie/:id' element={<ViewMovie />}></Route>
-        </Route>
-        <Route path='/add-movie' element={<AddMovieForm />}></Route>
-      </Routes>
+      <FavoriteMovieProvider>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<MoviesList />}>
+            <Route path='movie/:id' element={<ViewMovie />}></Route>
+          </Route>
+          <Route path='/add-movie' element={<AddMovieForm />}></Route>
+        </Routes>
+      </FavoriteMovieProvider>
     </div>
   );
 }
