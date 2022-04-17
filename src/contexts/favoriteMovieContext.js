d832c29703d343d30827acd6398551b624b45fb9
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-17 22:23:25 +0200

    create favorite movie context

---
+++ src/contexts/favoriteMovieContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react';
+
+export const FavoriteMoviesContext = createContext();
+export const UpdateFavoriteMoviesContext = createContext();
+
+export function FavoriteMovieProvider(props) {
+  const [favoriteMovies, setfavoriteMovies] = useState([2]);
+
+  const updateFavoriteMovies = (movieId) => {
+    if (favoriteMovies.includes(movieId)) {
+      const tmp = favoriteMovies.filter((id) => {
+        return movieId !== id ? movieId : null;
+      });
+      setfavoriteMovies(tmp);
+    } else {
+      setfavoriteMovies([...favoriteMovies, movieId]);
+    }
+  };
+
+  return (
+    <FavoriteMoviesContext.Provider value={favoriteMovies}>
+      <UpdateFavoriteMoviesContext.Provider value={updateFavoriteMovies}>
+        {props.children}
+      </UpdateFavoriteMoviesContext.Provider>
+    </FavoriteMoviesContext.Provider>
+  );
+}
