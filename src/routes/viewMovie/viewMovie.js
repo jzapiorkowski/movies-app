6648e852888c431f3dd6f59091fd4b61ae16e3f9
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-17 23:00:01 +0200

    add favorite icon to movie modal view with onClick function to add/remove

--- src/routes/viewMovie/viewMovie.js
+++ src/routes/viewMovie/viewMovie.js
@@ -1,15 +1,26 @@
 import { moviesClient } from '../../api/moviesClient';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './viewMovie.scss';
 import { AiOutlineClose } from 'react-icons/ai';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import {
+  FavoriteMoviesContext,
+  UpdateFavoriteMoviesContext,
+} from '../../contexts/favoriteMovieContext';
 
 export function ViewMovie() {
   let { id } = useParams();
   let navigate = useNavigate();
   const [movieInfo, setMovieInfo] = useState({});
+
+  const favoriteMoviesList = useContext(FavoriteMoviesContext);
+  const updateFavoriteMoviesContext = useContext(UpdateFavoriteMoviesContext);
+
+  const isFavorite = favoriteMoviesList.includes(movieInfo.id);
 
   useEffect(() => {
     moviesClient.get(`/movie/${id}`).then((response) => {
@@ -54,6 +65,17 @@ export function ViewMovie() {
           <p className='description'>{movieInfo.description}</p>
           <p onClick={ratingStars}>Directed by: {movieInfo.director}</p>
         </div>
+      </div>
+      <div className='favorite-icon'>
+        {isFavorite ? (
+          <FavoriteIcon
+            onClick={() => updateFavoriteMoviesContext(movieInfo.id)}
+          />
+        ) : (
+          <FavoriteBorderIcon
+            onClick={() => updateFavoriteMoviesContext(movieInfo.id)}
+          />
+        )}
       </div>
     </div>
   );
